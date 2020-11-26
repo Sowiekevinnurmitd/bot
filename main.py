a66e551c9b32b8709372c7dd73f18d001fb75f5d
Sowiekevinnurmitd <66833881+Sowiekevinnurmitd@users.noreply.github.com>
2020-11-26 10:36:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 from werkzeug.utils import redirect
 import nltk
+nltk.download('punkt')
 
 from nltk.stem.lancaster import LancasterStemmer
 
@@ -19,85 +20,38 @@ import pickle
 import json
 import random
 
-with open("intents_PE") as file:
+with open("intents.json") as file:
     data = json.load(file)
 
 
 
-try:
-    # hier x activieren, wenn etwas an der Json file geändert wurde, sonst nutzt es das schon trainieret Model
-    with open("data.pickle", "rb") as f:
-        words, labels, training, output = pickle.load(f)
-except:
-    words = []
-    labels = []
-    docs_x = []
-    docs_y = []
 
-    for intent in data['intents']:
-        for pattern in intent['patterns']:
-            wrds = nltk.word_tokenize(pattern)
-            words.extend(wrds)
-            docs_x.append(wrds)
-            docs_y.append(intent['tag'])
+# hier x activieren, wenn etwas an der Json file geändert wurde, sonst nutzt es das schon trainieret Model
+with open("data.pickle", "rb") as f:
+    words, labels, training, output = pickle.load(f)
 
-        if intent['tag'] not in labels:
-            labels.append(intent['tag'])
 
-    words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-    words = sorted(list(set(words)))
-
-    labels = sorted(labels)
-
-    training = []
-    output = []
-
-    out_empty = [0 for _ in range(len(labels))]
-
-    for x, doc in enumerate(docs_x):
-        bag = []
-
-        wrds = [stemmer.stem(w.lower()) for w in doc]
-
-        for w in words:
-            if w in wrds:
-                bag.append(1)
-
-            else:
-                bag.append(0)
-
-        output_row = out_empty[:]
-        output_row[labels.index(docs_y[x])] = 1
-
-        training.append(bag)
-        output.append(output_row)
-
-    training = numpy.array(training)  # numpy - takes lists and change them to arrays - format that is needed for model
-    output = numpy.array(output)
-
-    with open("data.pickle", "wb") as f:
-        pickle.dump((words, labels, training, output), f)
+with open("data.pickle", "wb") as f:
+    pickle.dump((words, labels, training, output), f)
 
 ########Setting up Model
 tf.reset_default_graph()  # Resetting the underlying  data graph
 
 net = tflearn.input_data(shape=[None, len(training[0])])  # Defines input shape we are expecting for the model
-net = tflearn.fully_connected(net, 8)  #  8 neurons for that hidden layer
+net = tflearn.fully_connected(net, 8)  # we have 8 neurons for that hidden layer
 net = tflearn.fully_connected(net, 8)  # another hidden layer
 net = tflearn.fully_connected(net, 8)  # another hidden layer
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, len(output[0]),
-                              activation="softmax")  # allows  to get probabilities for each output
+                              activation="softmax")  # allows us to get probabilities for each output
 net = tflearn.regression(net)  #
 
 model = tflearn.DNN(net)  # trains the model
 
-try:
-    model.load("model.tflearn")
-except:
-    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)  # pass all training data , epochs: amount of times it sees training data
-    model.save("model.tflearn")
+
+model.load("model.tflearn")
+
 
 
 def bag_of_words(s, words):
@@ -133,6 +87,7 @@ def chatbot():
     tag = labels[results_index]
 
     if results[results_index] > 0.8:
+        global answer
         for tg in data["intents"]:
             if tg['tag'] == tag:
                 responses = tg['responses']
